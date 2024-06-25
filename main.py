5cf23e2a2f301f44f280c34e77c72c70cc6d6a79
t-z-scott <tayzscott@gmail.com>
2024-06-25 12:12:57 -0700

    read & word_count methods

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
+    frank_path = "books/frankenstein.txt"
+    text = get_book_text(frank_path)
+    num_words = word_count(text)
+    print(f"{num_words} words found in the book")
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+def word_count(book):
+    words = book.split()
+    return len(words)
 
 main()
